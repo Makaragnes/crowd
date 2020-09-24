0ea605dd4d6887d5e2b11e75d09a821aff01e8da
makarahnes <64668971+Makaragnes@users.noreply.github.com>
2020-09-24 23:34:36 +0300

    Add files via upload

---
+++ cwd-auth-gateway-master/config.py
@@ -0,0 +1,21 @@
+GENERAL = {
+	'bc_login':'myrik833',
+	'wif':'5KNAFrwS2qUzFW2LmdY5Jds8JCWZQ1xrvxvQc5xLQbnGuJCtMnx',
+	'memo_wif':'5Hw2Ck3q1QUGb7apENRB9eBKc9tArTuGoDb17xqKfdHRaFjCNSd',
+	'telegram_bot_token':'1150160102:AAGhp3RSthoX71uBQuPldSe1yzwcC7kjcrw',
+	'ws':'wss://crowdwiz.biz/ws',
+	'pgsql_db':'',
+	'pgsql_db_host':'',
+	'pgsql_db_port':'',
+	'pgsql_db_user':'',
+	'pgsql_db_password':'',
+	'localdb_filename':'cwd_demo_bot.db',
+	'use_proxy':False,
+	'use_local_db':True,
+	'deposit_rate': 90,
+	'withdraw_rate': 100,
+}
+
+PROXIES = {
+		'https://api.telegram.org': 'socks5://127.0.0.1:9050',
+		}
